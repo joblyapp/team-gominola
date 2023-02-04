bf412cd77a163039b8821234de37347e066f5496
CarlosCruz1503 <carlosjose445566@gmail.com>
2023-02-04 15:27:06 -0500

    modal fixed

--- Front/src/components/containers/form.jsx
+++ Front/src/components/containers/form.jsx
@@ -155,7 +155,7 @@ const FormContact = () => {
                                                         <div class="modal-dialog">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
-                                                                    <h1 class="modal-title fs-5 text-black" id="exampleModalLabel">Gracias por hacer tu reserva, te esperamos</h1>
+                                                                    <h1 class="modal-title fs-5 text-black" id="exampleModalLabel">Gracias por hacer tú reserva, te esperamos</h1>
                                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                 </div>
                                                             </div>
